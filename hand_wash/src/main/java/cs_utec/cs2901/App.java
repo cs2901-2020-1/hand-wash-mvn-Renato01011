25a06c5f7030499feadb0601232551a714682361
TPD <renato.bacigalupo@utec.edu.pe>
2020-04-01 13:43:41 -0500

    hand wash

---
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -0,0 +1,15 @@
+package cs_utec.cs2901;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for (int i = 0; i < 100; i++) {
+		System.out.println( "Me debo lavar las manos por 20 segundos" );
+	}
+    }
+}
